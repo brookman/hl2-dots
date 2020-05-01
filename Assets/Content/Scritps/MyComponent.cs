70121f29de1a157771ca23c576b8b6712d5e33c3
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-05-01 15:26:33 +0200

    Add ECS and hybrid renderer.

---
+++ Assets/Content/Scritps/MyComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using Unity.Profiling;
+
+[GenerateAuthoringComponent]
+public struct MyComponent : IComponentData
+{
+    public float MyValue;
+}
---
+++ Assets/Content/Scritps/MySystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class MySystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var delta = Time.DeltaTime;
+        Entities.ForEach(
+            (ref Translation translation,
+                in MyComponent component) =>
+            {
+                translation.Value += new float3(delta * component.MyValue, 0, 0);
+            }).ScheduleParallel();
+    }
+}
